1a1dc7370e3e52b06101106f443d634d99dc531e
danielgutentag <s5dahein@stud.eah-jena.de>
2017-07-01 15:18:44 +0200

    Update LUT_logarithmisch.vhdl
    erstes grundgerüst erstellt

--- LUT_logarithmisch.vhdl
+++ LUT_logarithmisch.vhdl
@@ -1,1 +1,25 @@
---dieses file beinhaltet eine funktion, um aus einer rampe einen logarithmischen anstiech zu erzeugen 
+--dieses file beinhaltet eine funktion/zuordungsabelle, um aus einer rampe einen logarithmischen anstiech zu erzeugen 
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity lut_log is
+	port
+	(
+		--Eingang: Vektor 10bit
+    --Ausgang: Vektor 10bit
+	);
+end lut_log;
+
+architecture lut_log_algor of lut:log is
+begin
+	--Verhaltensbeschreibung auf algorithmischer Ebene
+end lut_log_algor;
+
+--architecture lut_log_regtrans of lut:log is
+--begin
+	--Verhaltensbeschreibung auf Register-Transfer-Ebene
+--end lut_log_regtrans;
+  
+  --statt die zweite architecture auszklammern, könnte man auch eine configuration nutzen
+  --ansonsten einfach "--" löschen, der compiler verwendet automatisch die letzte architecture
